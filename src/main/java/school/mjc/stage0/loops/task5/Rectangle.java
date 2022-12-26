ff03c04e216862275066a612539374db68522e36
Madara <ranel28021998@gmail.com>
2022-12-26 10:36:39 +0300

    Some fix

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -6,8 +6,18 @@ public class Rectangle {
     }
     public static void printRectangleFrom8s(int length, int height){
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < length; j++) {
-                System.out.print(8);
+            if ((i == 0) || (i == height - 1)) {
+                for (int j = 0; j < length; j++) {
+                    System.out.print(8);
+                }
+            } else {
+                for (int j = 0; j < length; j++) {
+                    if ((j == 0) || (j == length - 1)) {
+                        System.out.print(8);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
             }
             System.out.println();
         }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,9 +1,12 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+    public static void main(String[] args) {
+        printTriangle(5);
+    }
+    public static void printTriangle(int cathetusLength) {
         for (int i = 0; i < cathetusLength; i++) {
-            for (int j = 0; j <= cathetusLength; j++) {
+            for (int j = 0; j < cathetusLength; j++) {
                 if (j <= i) {
                     System.out.print(8);
                 } else {
